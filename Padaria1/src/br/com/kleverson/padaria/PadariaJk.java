86f0590a3081a95ce9415122b4ff8330a63a9e7e
kleversonsilva <jose.kleverson@dcx.ufpb.br>
2021-03-18 20:41:42 -0300

    Novalinha2 adcionada

--- Padaria1/src/br/com/kleverson/padaria/PadariaJk.java
+++ Padaria1/src/br/com/kleverson/padaria/PadariaJk.java
@@ -6,6 +6,7 @@ public class PadariaJk {
 		// TODO Auto-generated method stub
 		System.out.println("Sejam Todos Bem Vindos !! ");
 		System.out.println("Na padaria JK vc encontra tudo em variedades !! ");
+		System.out.println("Temos também uma parte de farmácia!! ");
 	}
 
 }
