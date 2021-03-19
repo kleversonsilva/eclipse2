6c8859252ad6ab3ffc4a089998e3b628a1078e2f
kleversonsilva <jose.kleverson@dcx.ufpb.br>
2021-03-18 21:04:46 -0300

    Mais uma linha03

--- Padaria1/src/br/com/kleverson/padaria/PadariaJk.java
+++ Padaria1/src/br/com/kleverson/padaria/PadariaJk.java
@@ -8,6 +8,8 @@ public class PadariaJk {
 		System.out.println("Na padaria JK vc encontra tudo em variedades !! ");
 		System.out.println("Temos também uma parte de farmácia!! ");
 		System.out.println("ULTIMA LINHA ADCIONADA com SUCESSO 1-2-3-4!!! ");
+		System.out.println("Fim...  ");
+		
 		
 		
 		
